ea893aca87766ff46487ecb996d1f4a623390f0a
supratim17 <supratim.mondal17@gmail.com>
2015-08-28 15:27:58 +0530

    [PART 3]  - Added two new templates details.html & index.html  - Data being fetched from database  - Added namespace 'polls'

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
+from .models import Question, Choice
 
-# Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+   	response = "You're looking at the results of question %s."
+    try: 
+	    p = get_object_or_404(Question, pk=question_id)
+	    return HttpResponse(str(p.choice_set))
+    except Question.DoesNotExist:
+	    return 0	
+    
+    #return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+def answer(request, qid):
+	return HttpResponse("You're answering on question %s." % qid)
